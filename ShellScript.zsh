69d289f135bc23ad34ac7fb961a5096c394f650b
Panos Kolyvakis <114179217+PanosKolyvakis@users.noreply.github.com>
2024-01-19 11:21:42 +0200

    Update ShellScript.zsh

--- ShellScript.zsh
+++ ShellScript.zsh
@@ -1,27 +1,32 @@
 #!/bin/zsh
 
+# Add Conda to PATH (the user needs to replace the placeholder with the actual path to their Conda bin directory)
+export PATH="/path/to/conda/bin:$PATH"
 
-# Add Conda to PATH
-export PATH="/Users/panoskolyvakis/opt/miniconda3/bin:/opt/homebrew/bin:$PATH"
+# Source the Conda configuration (the user needs to replace the placeholder with the actual path to their conda.sh)
+source /path/to/conda/etc/profile.d/conda.sh
+# Activate the Conda environment (the user should replace 'your_env' with the name of their Conda environment)
+conda activate your_env
 
-
-
-# Activate your Conda environment
-source /Users/panoskolyvakis/opt/miniconda3/etc/profile.d/conda.sh
-conda activate ImageToText
-
-screenshot_directory="/Users/panoskolyvakis/VSprojects/ImageToText"
+# Set the directory where you want to save the screenshot and output file
+# The user should replace these placeholders with the paths where they want to save screenshots and outputs
+screenshot_directory="path/to/directory/for/screenshot"
 screenshot_filename="screenshot.png"
+output_filename="output.txt"
+
+# Take a screenshot and save it to the specified directory
 screencapture -i -t png "$screenshot_directory/$screenshot_filename"
-output_filename="output.txt" 
 
 # Check if the user canceled or captured the screenshot
 if [ $? -eq 0 ]; then
     echo "Screenshot saved successfully."
 
-    # Call your Python script to process the screenshot
-    python3 /Users/panoskolyvakis/VSprojects/ImageToText/main.py > "$screenshot_directory/$output_filename"
-	open -a TextEdit "$screenshot_directory/$output_filename"
+    # Call the Python script to process the screenshot
+    # The user should replace 'path/to/python/script' with the path to their Python script
+    python3 path/to/python/script/main.py > "$screenshot_directory/$output_filename"
+    
+    # Open the output text file in TextEdit or any other preferred text editor
+    open -a TextEdit "$screenshot_directory/$output_filename"
 else
     echo "Screenshot capture canceled."
 fi
