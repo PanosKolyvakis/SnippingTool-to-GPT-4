916c99e48389c3c7b35e5f79405f0459f17ad3b0
Panos Kolyvakis <114179217+PanosKolyvakis@users.noreply.github.com>
2024-02-02 00:01:06 +0200

    adjusted to include GPT-4-vision

--- main.py
+++ main.py
@@ -4,6 +4,9 @@ from gui import run_gui
 from Config import PathConfig
 from IDE import main_s
 import pytesseract  
+from ImageDescription import ImDescribe , display_gui
+
+path_config = PathConfig()
 
 def process_image_with_ocr(image_path):
     image = Image.open(image_path)
@@ -30,27 +33,33 @@ def get_response_from_openai_api(extracted_text, prompt, selected_model):
 if __name__ == "__main__":
 
     PathConfig = PathConfig()
-    
+    vision_model = 'gpt-4-vision-preview'
+
     extracted_text = process_image_with_ocr(PathConfig.screenshot_path)
-    if extracted_text:
+    extracted_text = extracted_text if extracted_text else ' '
 
-        # Write the extracted text to the OCR output path
-        with open(PathConfig.ocr_output_path, 'w') as file:
-            file.write(extracted_text)
+    # Write the extracted text to the OCR output path
+    with open(PathConfig.ocr_output_path, 'w') as file:
+        file.write(extracted_text)
 
-        selected_prompt, selected_model = run_gui()
+    # run the GUI for the prompt and model selection
+    selected_prompt, selected_model = run_gui()
+    
+    # this code gets executed if 
+    if selected_prompt and selected_model!= vision_model:
 
-        if selected_prompt:
-            output_text = get_response_from_openai_api(extracted_text, selected_prompt, selected_model)
-
-
-            
+        output_text = get_response_from_openai_api(extracted_text, selected_prompt, selected_model)
         with open(PathConfig.api_output_path, 'w') as file:
-            
+        
             file.write(output_text)
 
-
         main_s()
+
+    # this code gets executed if the Vision model is selected and it will generate a description of the image
     else:
-        pass 
+        display_gui(path_config.screenshot_path, ImDescribe())
+    
+
+
+    
 
